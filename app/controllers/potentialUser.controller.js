a84026b4dd2ce4b1113d169093c7a6ff4c986818
Carlos Diez <cdiezmoran@gmail.com>
2017-02-14 18:46:46 -0800

    Added access headers

--- app/controllers/potentialUser.controller.js
+++ app/controllers/potentialUser.controller.js
@@ -1,6 +1,8 @@
 var User = require('../models/potentialUser.js');
 
 exports.addPotentialUser = function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   var newUser = new User(req.body.user);
 
   User.findOne({ 'email': newUser.email }).exec(function(err, user) {
