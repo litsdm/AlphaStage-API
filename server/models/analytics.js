7768575248889413e8216895eb2e9a785f925ff2
Carlos Diez <cdiezmoran@gmail.com>
2017-04-22 18:59:59 -0700

    Modify analytics model

--- server/models/analytics.js
+++ server/models/analytics.js
@@ -5,7 +5,12 @@ var AnalyticsSchema = new Schema({
   players: { type: Number, default: 0 },
   sessions: { type: Number, default: 0 },
   pageViews: { type: Number, default: 0 },
-  impressions: { type: Number, default: 0 }
+  impressions: { type: Number, default: 0 },
+  downloads: { type: Number, default: 0 },
+  pageViewUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  downloadUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  playingUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  uninstallUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
 
 module.exports = mongoose.model('Analytics', AnalyticsSchema);
